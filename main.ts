d541ef03c7fc1d5188a5e6f2c31a6102ba7e5506
benjaminlin09 <70210242+benjaminlin09@users.noreply.github.com>
2020-08-25 21:08:30 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+let Number2 = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        Number2 += 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        Number2 += -1
+    }
+    if (Number2 == -1) {
+        Number2 = 0
+    }
+    if (Number2 == 21) {
+        Number2 = 20
+    }
+    if (Number2 == 0) {
+        basic.showNumber(0)
+    }
+    if (Number2 == 1) {
+        basic.showNumber(1)
+    }
+    if (Number2 == 2) {
+    	
+    }
+    if (Number2 == 3) {
+    	
+    }
+    if (Number2 == 4) {
+    	
+    }
+    if (Number2 == 5) {
+    	
+    }
+    if (Number2 == 6) {
+    	
+    }
+    if (Number2 == 7) {
+    	
+    }
+    if (Number2 == 8) {
+    	
+    }
+    if (Number2 == 9) {
+    	
+    }
+    if (Number2 == 10) {
+    	
+    }
+    if (Number2 == 11) {
+    	
+    }
+    if (Number2 == 12) {
+    	
+    }
+    if (Number2 == 13) {
+    	
+    }
+    if (Number2 == 14) {
+    	
+    }
+    if (Number2 == 15) {
+    	
+    }
+    if (Number2 == 16) {
+    	
+    }
+    if (Number2 == 17) {
+    	
+    }
+    if (Number2 == 18) {
+    	
+    }
+    if (Number2 == 19) {
+    	
+    }
+    if (Number2 == 20) {
+    	
+    }
+})
